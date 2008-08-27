ab8d9ae5f6db27afd0cb822b8e48377cb8201cde
David Nadlinger <klickverbot@gmail.com>
2008-08-28 00:50:00 +0200

    Updated the CollectionFields-mixin to handle a missing existing_#{ name }_attributes_hash in mass-assignment attributes properly.

--- config/initializers/collection_fields.rb
+++ config/initializers/collection_fields.rb
@@ -3,8 +3,17 @@
 #
 # We save the elements of the association not until the main object is saved, 
 # because otherwise they would be saved/changed even if a validation on the 
-# main object failed.
+# main object failed and it didn't get saved.
 #
+# We have to make sure that the existing_#{ name }_attributes hash is set on
+# every mass assign operation, even if it is not passed in the attributes
+# (i.e. if all existing_#{ name }_attributes-fields were removed from the form),
+# because otherwise the relations would not be removed properly.
+#
+# In existing_#{ name }_attributes= we check if the collection actually exists.
+# We do this in order to support non-ActiveRecord::AssociationCollection
+# attributes, e.g. in models without database table, for those are not
+# automatically initialized with an empty array and thus might be nil.
 
 module CollectionFields
   module Base
@@ -22,11 +31,17 @@ module CollectionFields
               element.save( false )
             end
           end
+          
+          def attributes_with_#{ name }=( new_attributes, *args )
+            self.send( :attributes_without_#{ name }=, new_attributes.with_indifferent_access.reverse_merge( 'existing_#{ name }_attributes' => {} ), *args )
+          end
+          alias_method_chain :attributes=, :#{ name }
         END_OF_EVAL
         
+        # Check if there is a custom build proc set, otherwise use the 
+        # faster class_eval approach.
         if options[ :build_new ]
           define_method "new_#{ name }_attributes=" do |collection_elements|
-            instance_variable_set( "@#{ name }", [] ) unless self.send( name )
             collection_elements.each do |index, element_attributes|
               options[ :build_new ].call( self, element_attributes )
             end
@@ -41,9 +56,12 @@ module CollectionFields
           END_OF_EVAL
         end
         
+        # Check if there is a custom update proc set, otherwise use the 
+        # faster class_eval approach.
         if options[ :update_existing ]
           define_method "existing_#{ name }_attributes=" do |collection_elements|
             collection = self.send( name )
+            return unless collection
             collection.reject( &:new_record? ).each do |element|
               element_attributes = collection_elements[ element.id.to_s ]
               if element_attributes
@@ -56,6 +74,7 @@ module CollectionFields
         else
           class_eval <<-END_OF_EVAL, __FILE__, __LINE__
             def existing_#{ name }_attributes=( collection_elements )
+              return unless self.#{ name }
               self.#{ name }.reject( &:new_record? ).each do |element|
                 element_attributes = collection_elements[ element.id.to_s ]
                 if element_attributes
