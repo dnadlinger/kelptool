858f47f29a6333ea9e71b332a86c2bc01e842665
David Nadlinger <klickverbot@gmail.com>
2008-08-28 01:56:51 +0200

    Reworked PhoneNumberTypes controller and view to comply with the project standards.

--- app/controllers/phone_number_types_controller.rb
+++ app/controllers/phone_number_types_controller.rb
@@ -1,85 +1,46 @@
 class PhoneNumberTypesController < ApplicationController
-  # GET /phone_number_types
-  # GET /phone_number_types.xml
   def index
-    @phone_number_types = PhoneNumberType.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @phone_number_types }
-    end
+    @phone_number_types = PhoneNumberType.find( :all, :order => 'name' )
   end
 
-  # GET /phone_number_types/1
-  # GET /phone_number_types/1.xml
   def show
-    @phone_number_type = PhoneNumberType.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @phone_number_type }
-    end
+    @phone_number_type = PhoneNumberType.find( params[ :id ] )
   end
 
-  # GET /phone_number_types/new
-  # GET /phone_number_types/new.xml
   def new
     @phone_number_type = PhoneNumberType.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @phone_number_type }
-    end
   end
 
-  # GET /phone_number_types/1/edit
   def edit
-    @phone_number_type = PhoneNumberType.find(params[:id])
+    @phone_number_type = PhoneNumberType.find( params[ :id ] )
   end
 
-  # POST /phone_number_types
-  # POST /phone_number_types.xml
   def create
-    @phone_number_type = PhoneNumberType.new(params[:phone_number_type])
+    @phone_number_type = PhoneNumberType.new( params[ :phone_number_type ] )
 
-    respond_to do |format|
-      if @phone_number_type.save
-        flash[:notice] = 'Telefonnummer-Typ hinzugefügt.'
-        format.html { redirect_to phone_number_types_url }
-        format.xml  { render :xml => @phone_number_type, :status => :created, :location => @phone_number_type }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @phone_number_type.errors, :status => :unprocessable_entity }
-      end
+    if @phone_number_type.save
+      flash[ :notice ] = 'Telefonnummer-Kategorie hinzugefügt.'
+      redirect_to :action => 'index'
+    else
+      render :action => 'new'
     end
   end
 
-  # PUT /phone_number_types/1
-  # PUT /phone_number_types/1.xml
   def update
-    @phone_number_type = PhoneNumberType.find(params[:id])
+    @phone_number_type = PhoneNumberType.find( params[ :id ] )
 
-    respond_to do |format|
-      if @phone_number_type.update_attributes(params[:phone_number_type])
-        flash[:notice] = 'Änderungen gespeichert.'
-        format.html { redirect_to phone_number_types_url }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @phone_number_type.errors, :status => :unprocessable_entity }
-      end
+    if @phone_number_type.update_attributes( params[ :phone_number_type ] )
+      flash[ :notice ] = 'Änderungen gespeichert.'
+      redirect_to :action => 'index'
+    else
+      render :action => 'edit'
     end
   end
 
-  # DELETE /phone_number_types/1
-  # DELETE /phone_number_types/1.xml
   def destroy
-    @phone_number_type = PhoneNumberType.find(params[:id])
+    @phone_number_type = PhoneNumberType.find( params[ :id ] )
     @phone_number_type.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(phone_number_types_url) }
-      format.xml  { head :ok }
-    end
+    flash[ :notice ] = 'Telefonnummer-Kategorie gelöscht.'
+    redirect_to :action => 'index'
   end
 end
